84d64944fa719e7b31cbd06f84772ba560959318
berte <behzaterte@yandex.com>
2018-02-19 12:39:49 +0300

    task01-02 is done, waiting code review

---
+++ cmd/ufs.c
@@ -0,0 +1,47 @@
+/*
+ * berte <behzaterte at yandex.com>
+ *
+ * ffs support
+ */
+
+#include <fs.h>
+
+static int do_ffsls(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
+{
+    return do_ls(cmdtp, flag, argc, argv, FS_TYPE_FFS);
+}
+
+int do_ffsload(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
+{
+	return do_load(cmdtp, flag, argc, argv, FS_TYPE_FFS);
+}
+
+int do_ffssize(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
+{
+	return do_size(cmdtp, flag, argc, argv, FS_TYPE_FFS);
+}
+
+U_BOOT_CMD(
+	ffssize,       4,      0,      do_ffssize,
+	"determine a file's size",
+        "<interface> <dev[:part]> <filename>\n"
+        "    - Find file 'filename' from 'dev' on 'interface'\n"
+	"      and determine its size."
+);
+
+
+
+U_BOOT_CMD(
+	ffsls,	4,	1,	do_ffsls,
+	"list files in a directory (default /)",
+	"<interface> <dev[:part]> [directory]\n"
+	"    - list files from 'dev' on 'interface' in a 'directory'"
+)
+
+U_BOOT_CMD(
+	ffsload,	6,	0,	do_ffsload,
+	"load binary file from a ffs filesystem",
+	"<interface> [<dev[:part]> [addr [filename [bytes [pos]]]]]\n"
+	"    - load binary file 'filename' from 'dev' on 'interface'\n"
+	"      to address 'addr' from ffs filesystem."
+)
